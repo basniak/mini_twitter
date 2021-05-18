449b57c8d4bac80922bfe8fe13d8dd1889bcd95d
Vinicius Basniak <vinicius.basniak@gmail.com>
2021-05-18 01:16:10 -0300

    querys

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000;
+const db = require('./banco/querys')
 
 app.use(bodyParser.json())
 app.use(
@@ -14,7 +17,17 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API 🐱‍🏍' })
 })
 
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+
 app.listen(port, () => {
  console.log(`App running on port ${port} 🐱‍🏍.`)
-  console.log(`http://localhost:${port}.`)
+  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${port}/users`)
+  console.log(`http://localhost:${port}/users/01`)
 })
+
